f4b9f382013ba7aa6d9ab57464e6c39518824dc5
Harish Thangaraj <harish.thangaraj03@outlook.com>
2026-09-22 09:50:21 -0700

    changed uvicorn start to root

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "app.main:app",
+        app,
         host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", "8000")),
     )
